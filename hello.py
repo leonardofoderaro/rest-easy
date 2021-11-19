9208d97c67b74ead6d1a1bda0f73270f64c9698e
Leonardo Foderaro <startrack79@gmail.com>
2021-11-19 12:33:53 +0100

    added support for personalized message

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 import hug
 
-@hug.get('/hello')
+@hug.get('/hello', versions=1)
 def hello():
+    """Returns a generic 'Hello World' message"""
     return {'message': 'Hello World!'}
+
+@hug.get('/hello', versions=2)
+def helloName(name):
+    """Returns a custom message using the name passed as parameter, or an error if it's missing"""
+    return {'message': 'Hello {}!'.format(name)}
